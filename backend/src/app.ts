47ebc0989e666a7470cd90575cc6668838c0acaa
Nikita Kalanchekaev <nikita.kalan@yandex.ru>
2026-02-06 03:59:12 +0300

    fixed limiter

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -19,7 +19,7 @@ app.set('trust proxy', true)
 
 const limiter = rateLimit({
     windowMs: 10 * 60 * 1000,
-    max: 10,
+    max: 50,
     standardHeaders: true,
     legacyHeaders: false,
     validate: { trustProxy: false }
